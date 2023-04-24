cc8dd678f00dac876f9e7ff6172e69224758cc64
EdiGabriel22 <edigabrielcontato@hotmail.com>
2023-04-24 19:43:41 -0300

    criando lista de negociações

--- Alura/alurabank/app/controllers/negociacao-controller.ts
+++ Alura/alurabank/app/controllers/negociacao-controller.ts
@@ -1,9 +1,11 @@
+import { Negociacoes } from './../models/negociacoes.js';
 import { Negociacao } from './../models/negociacao.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement
     private InputQuantidade: HTMLInputElement
     private inputValor: HTMLInputElement
+    private negociacoes: Negociacoes = new Negociacoes()
 
     constructor() {
         this.inputData = document.querySelector("#data")
@@ -11,14 +13,25 @@ export class NegociacaoController {
         this.inputValor = document.querySelector("#valor")
     }
 
-    adiciona() {
+    adiciona(): void {
+        const negociacao = this.criaNegociacao()
+        this.negociacoes.adicona(negociacao)
+        console.log(this.negociacoes.lista())
+        this.limparFormulario()
+    }
+
+    criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.InputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
+        return new Negociacao( date, quantidade, valor )
+    }
 
-        const negociacao = new Negociacao( date, quantidade, valor )
-
-        console.log(negociacao)
+    limparFormulario(): void {
+        this.inputData.value = ""
+        this.InputQuantidade.value = ""
+        this.inputValor.value = ""
+        this.inputData.focus()
     }
 }
